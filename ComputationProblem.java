320517a125f82a72c091abb4d883f03396597bfb
unknown <sam1919>
2021-05-13 00:20:11 +0530

    Calculating Wages for a Month

--- ComputationProblem.java
+++ ComputationProblem.java
@@ -5,6 +5,7 @@ public class ComputationProblem
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int COUNT_OF_WORK_DAYS = 2;
 	
 	public static void main(String[] args)
 	{		
@@ -12,24 +13,29 @@ public class ComputationProblem
 
 	int empHrs = 0;
 	int empWage = 0;
-	int empCheck = (int)Math.floor(Math.random() * 10) % 3;
-	switch (empCheck) 
+	int totalEmpWage = 0;
+	
+	for (int day = 0; day < COUNT_OF_WORK_DAYS;day++)
 	{
+		int empCheck = (int)Math.floor(Math.random() * 10) % 3;
+		switch (empCheck) 
+		{
 	
-		case IS_PART_TIME:
-			empHrs =4;
-			break;
+			case IS_PART_TIME:
+				empHrs =4;
+				break;
 		
-		case IS_FULL_TIME:
-			empHrs = 8;
-			break;
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
 		
-		default:
-		empHrs = 0;
-	}
+			default:
+				empHrs = 0;
+		}
 	empWage = empHrs * 	EMP_RATE_PER_HOUR;
-		System.out.println("Employee wage:" + empWage);
+	totalEmpWage += empWage;	
+	System.out.println("Employee wage:" + empWage);
 		
-	
+	}
 	}
 }
