3d53d0ef2f2d64a538bde72a13990a9e80852acd
unknown <sam1919>
2021-05-12 11:46:49 +0530

    Welcome to Employee Wage Computation Program

---
+++ ComputationProblem.java
@@ -0,0 +1,9 @@
+package com.empwagecomputaionprogram;
+
+public class ComputationProblem 
+{
+public static void main(String[] args) 
+{
+	System.out.println("Welcome to Employee Wage Computation Program");
+}
+}
