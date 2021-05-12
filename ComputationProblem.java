738f79e7696d0268f30c89231648a59f23024706
unknown <sam1919>
2021-05-12 11:55:30 +0530

    Check Emp is Present or Absent

--- ComputationProblem.java
+++ ComputationProblem.java
@@ -5,5 +5,17 @@ public class ComputationProblem
 public static void main(String[] args) 
 {
 	System.out.println("Welcome to Employee Wage Computation Program");
+	
+	int x = 1;
+	double empCheck = Math.floor(Math.random() * 10) % 2;
+	if ( empCheck == x )
+	{
+		System.out.println("Employee is Present");
+	}
+	else 
+		{
+		System.out.println("Employee is Absent");
+		}
+	
 }
 }
