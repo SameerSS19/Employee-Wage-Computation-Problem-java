45855ae05d0b9783a90288b8d801c696e7130984
SameerSS19 <sk3090019@gmail.com>
2021-05-17 20:36:26 +0530

    Employee is Present or Absent

--- ComputationProblem.java
+++ ComputationProblem.java
@@ -5,7 +5,6 @@ public class ComputationProblem
 public static void main(String[] args) 
 {
 	System.out.println("Welcome to Employee Wage Computation Program");
-	
 	int x = 1;
 	double empCheck = Math.floor(Math.random() * 10) % 2;
 	if ( empCheck == x )
